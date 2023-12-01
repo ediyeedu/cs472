662a2b20944e6645fef88f162bd8da4b7f89042e
ediyedu <ediyeedu@gmail.com>
2023-11-30 22:44:26 -0600

    added

---
+++ new/doc.js
@@ -0,0 +1,20 @@
+/*function foo(){
+    console.log(arguments);
+}
+function foo(arg1){
+    console.log(arg1);
+}
+function foo(arg1,arg2){
+    console.log(arg1,arg2);
+}
+function foo(arg1,arg2,arg3){
+    console.log(arg1,arg2,arg3);
+}
+foo();
+foo(1);
+foo(1,2);
+foo(1,2,3);*/
+const str = 'Hello';
+const arr = [...str];
+console.log(arr);
+

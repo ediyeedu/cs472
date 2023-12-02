f094e4959ed087cc11b578955bfc0bebf5f4ab2a
ediyedu <ediyeedu@gmail.com>
2023-12-02 17:23:24 -0600

    added

--- new/doc.js
+++ new/doc.js
@@ -19,13 +19,49 @@ const arr = [...str];
 console.log(arr);*/
 
 //console.log("hHello world")
-alert("example of alert")
-console.log(typeof true)
-console.log(typeof null)
-console.log(typeof undefined)
-console.log(typeof {})
-console.log(typeof [])
-a=5
-console.log(a)
-var a = 10;
-console.log(a)
+// alert("example of alert")
+// console.log(typeof true)
+// console.log(typeof null)
+// console.log(typeof undefined)
+// console.log(typeof {})
+// console.log(typeof [])
+// a=5
+// console.log(a)
+// var a = 10;
+// console.log(a)
+
+// const x=5;
+// console.log(x);
+// if(x==5){
+//     var y=2*x;
+//     let x=4;
+//     console.log(x);
+//     console.log(y);
+// }
+// console.log(y);
+// console.log(x);
+// function sum(x,y){
+//     return x+y;
+// }
+// console.log(sum());
+// let x = 10;
+// function main() {
+// let x;
+// console.log("x1: " + x);
+// if (x > 0) {
+// let x = 30;
+// console.log("x2: " + x);
+// }
+// x= 40;
+// let f = function(x) { console.log("x3: " + x); }
+// f(50);
+// }
+// main();
+    function order(a,b){
+        if(a>b) return 1;
+        if(a==b) return 0;
+        if(a<b) return -1;
+    }
+    let arr = [9,2,8,4];
+arr.sort(order);
+console.log(arr);
